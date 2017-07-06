ff2cd2361c965d4c33f06fb7496f9f2ee2afd0c2
Charles Rudolph <charles.rudolph@originate.com>
2017-07-06 14:16:30 -0700

    javascript exorelay: update dependency cucumber

--- javascript/exorelay/cucumber.js
+++ javascript/exorelay/cucumber.js
@@ -1,3 +1,11 @@
+var formatOptions = JSON.stringify({snippetSyntax: 'node_modules/cucumber-snippets-livescript'})
+
+var common = [
+  '--compiler', 'ls:livescript',
+  '--format-options', '\'' + formatOptions + '\'',
+  '--require', 'features'
+].join(' ')
+
 module.exports = {
-  "default": "--compiler ls:livescript -r features --strict --snippet-syntax node_modules/cucumber-snippets-livescript"
+  "default": common
 }
--- javascript/exorelay/features/support/env.ls
+++ javascript/exorelay/features/support/env.ls
@@ -1,12 +1,13 @@
-module.exports = ->
+require! {
+  'cucumber': {After, Before, set-default-timeout}
+}
 
-  @set-default-timeout 1000
-
-
-  @After ->
-    @exo-relay?.close!
-    @exocom?.close!
+set-default-timeout 1000
 
 
-  @Before ->
-    @ran = no
+After ->
+  @exo-relay?.close!
+  @exocom?.close!
+
+Before ->
+  @ran = no

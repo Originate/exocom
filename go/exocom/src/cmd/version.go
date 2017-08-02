667afbdce90166517390662cbeabf1dc6f4b1e12
Charlie Rudolph <charles.w.rudolph@gmail.com>
2017-08-02 09:49:35 -0700

    update publish script (#185)

---
+++ go/exocom/src/cmd/version.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Displays the version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("0.22.1")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}

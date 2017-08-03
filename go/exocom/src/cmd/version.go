eca1d14b0df042c121b0976d23dd970c16c5e46b
Charles Rudolph <charles.rudolph@originate.com>
2017-08-03 14:22:19 -0700

    0.23.0

--- go/exocom/src/cmd/version.go
+++ go/exocom/src/cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Displays the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("0.22.1")
+		fmt.Println("0.23.0")
 	},
 }
 

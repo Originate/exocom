65d1d3c8e2a6eb3ec1d6c49f94c32559ad25d2ca
Charlie Rudolph <charles.w.rudolph@gmail.com>
2017-08-08 08:48:27 -0700

    stabilize message cache test (#214)

--- go/exocom/src/exocom/message_cache_test.go
+++ go/exocom/src/exocom/message_cache_test.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/Originate/exocom/go/exocom/src/exocom"
+	"github.com/Originate/exocom/go/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -13,10 +14,12 @@ var _ = Describe("MessageCache", func() {
 		messageCache *exocom.MessageCache
 		timestamp    time.Time
 	)
+
 	BeforeEach(func() {
 		messageCache = exocom.NewMessageCache(time.Millisecond)
 		timestamp = time.Now()
 	})
+
 	Describe("Get / Set", func() {
 		It("returns the timestamp of the message with the given activityId", func() {
 			messageCache.Set("message1", timestamp)
@@ -29,20 +32,22 @@ var _ = Describe("MessageCache", func() {
 			Expect(ok).To(BeFalse())
 		})
 	})
+
 	Describe("clearCache", func() {
 		It("clears the messageCache from messages older than one minute", func() {
 			messageCache.Set("message1", time.Now().Add(time.Minute*-1))
-			time.Sleep(time.Millisecond * 5)
-			_, ok := messageCache.Get("message1")
-			Expect(ok).To(BeFalse())
+			utils.WaitFor(func() bool {
+				_, ok := messageCache.Get("message1")
+				return !ok
+			}, "expected message1 to be cleared from the cache but it never was")
 		})
+
 		It("keeps messages in the messageCache that are younger than a minute", func() {
 			messageCache.Set("message2", timestamp)
-			time.Sleep(time.Millisecond * 10)
+			time.Sleep(time.Millisecond * 100)
 			result, ok := messageCache.Get("message2")
 			Expect(ok).To(BeTrue())
 			Expect(result).To(Equal(timestamp))
 		})
 	})
-
 })
